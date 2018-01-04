fa4a34e43a0a84cb7c5307b68e27b4adce30984c
A I <atbisrael@gmail.com>
2018-01-04 07:17:26 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,12 +2,14 @@ def fizzbuzz(user_input)
   
     if user_input % 3 == 0
       puts "Fizz"
-    else if user_input % 5 == 0
-      puts "Buzz"
-    else if user_input % 3 == 0 && user_input % 5 == 0
-      puts "FizzBuzz"
-    else 
-      puts nil
+    end
   end
-end
+#     else if user_input % 5 == 0
+#       puts "Buzz"
+#     else if user_input % 3 == 0 && user_input % 5 == 0
+#       puts "FizzBuzz"
+#     else 
+#       puts nil
+#   end
+# end
 
